50e1285eeadea4dc9367aae25ca231c5afe7ee50
Tina <Tina.saleh.888@gmail.com>
2024-09-30 14:37:17 +0200

    Uppgift2

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import { Image, StyleSheet, Platform, Text, View, Button, TextInput } from 'react-native';
 export default function Index() {
-  const [counter, setCounter] = useState(0);
-  function dosomeplus() {
+  const [counter, setCounter] = useState(0); // we define the constant 
+
+  // we define 3 functions, plus, minus and reset
+  
+  function dosomeplus() {                   
       setCounter(counter + 1);
   }
   function dosomeminus() {
@@ -14,6 +17,7 @@ export default function Index() {
   function reset () {
     setCounter(0);
   }
+  // we style the buttons and decide buttons location in the screen
   return (
     <View
       style={{
@@ -23,24 +27,26 @@ export default function Index() {
       }}
     >
       <Text style= {{fontSize: 150}}>{counter}</Text>
-      <View style={{ flexDirection:"row" }}>
-      <View style={{ backgroundColor: "darkgreen", width : 175, height: 60, marginEnd: 5, justifyContent: 'center', alignItems: "center", marginBottom: 10}}>
+     
+      <View style={{ backgroundColor: "green", width : 155, height: 50, justifyContent: 'center', alignItems: "center", marginBottom: 10}}>
       <Button
         onPress={dosomeplus}
         title="PLUS"
         color="white"
       />
       </View>
-      <View style={{ backgroundColor: "red", width : 175, height: 60, justifyContent: 'center', alignItems: "center"}}>
+      <View style={{ backgroundColor: "red", width : 155, height: 50, justifyContent: 'center', alignItems: "center", marginBottom: 15}}>
       <Button
         onPress={dosomeminus}
         title="MINUS"
         color="white"
       />
+      
       </View>
-      </View>
+
+
       {counter > 0 && (
-      <View style={{ backgroundColor: "blue", width : 175, height: 40, justifyContent: 'center', alignItems: "center"}}>
+      <View style={{ backgroundColor: "cyan", width : 175, height: 40, justifyContent: 'center', alignItems: "center"}}>
       <Button
         onPress={reset}
         title="Reset"
